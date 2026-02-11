e2e8bace07e447fc1977d180080ef2d6a9baeb20
Tobias Pfennig <pfennig@ipk-gatersleben.de>
2026-02-11 10:13:19 +0100

    updated fasta header extraction function for efficient bulk action

--- utils.py
+++ utils.py
@@ -4,80 +4,12 @@ import re
 import pandas as pd
 import numpy as np
 
+from functools import partial
+
 from typing import Union
 
 # Function to extract information from FASTA-like headers
-def extract_fasta_header_like(header:str, key_prefix:str="", chunk_suffix:Union[bool,None]=None)->pd.Series:
-    """Function to extract information in a FASTA-like header as produced by the "extract_genes" functions.
-
-    Args:
-        header (str): Header of a sequence
-        key_prefix (str, optional): String to be added to the beginning of the output indices. Defaults to "".
-        chunk_suffix (Union[bool,None], optional): Does the header contain a suffix indicating the chunk position produced by the DeepCistrome function 'split_sequence_chunks_to_250bp'. If None, the suffix is automatically detected. Defaults to None.
-
-    Raises:
-        ValueError: If the header structure dos not follow the convention '[>]<_id> [<key>=<value>] ... [<key>=<value>][_<chunk_suffix>]'.
-
-    Returns:
-        pd.Series: A Series object storing the header information as key:value pairs.
-    """
-    # Remove the initial ">"
-    header= header.strip()
-    if header.startswith(">"):
-        header = header[1:]
-
-    # Initialize an info Series to save the header information
-    info = pd.Series(dtype=object)
-
-    if chunk_suffix is None:
-        chunk_suffix = re.search("_chunk[0-9_\-]+$", header)
-
-    # If there is a chunk suffix, extract it
-    if chunk_suffix:
-        header, chunk_range = header.split("_chunk")
-        chunk_start, chunk_end = np.array(chunk_range.split("_")[1].split("-")).astype(int)
-
-
-    # Split the header by spaces
-    header_spc = header.split(" ")
-
-    # The ID is the first element
-    id = header_spc.pop(0)
-    info[key_prefix + "_id"] = id
-
-    # Initialise a string to hold the current piece of information
-    curr_info = []
-
-    for i, header_field in enumerate(header_spc):
-        # Add the new field to the current info
-        curr_info.append(header_field)
-
-        # If the next field includes a "=", the current info is done
-        if len(header_spc) == i+1 or re.search("=", header_spc[i+1]):
-            # Combine the current information to a string
-            curr_info_str = " ".join(curr_info)
-
-            if len(info) == 1 and not re.search("=", curr_info_str):
-                info_key = "_names"
-                info_val = curr_info_str
-            elif re.search("=", curr_info_str):
-                info_key, info_val = curr_info_str.split("=")
-            else:
-                raise ValueError(f"Unrecognized fasta header structure.\n<key>=<value> par expected in {curr_info_str}")
-
-            # Add the current info to the dict
-            info.loc[key_prefix + info_key] = info_val
-
-            # Reset the current information list
-            curr_info = []
-
-    if chunk_suffix:
-        info[key_prefix + "_chunk_start"] = chunk_start
-        info[key_prefix + "_chunk_end"] = chunk_end
-
-    return info
-
-def extract_fasta_header_like_series(header:Union[str,list,pd.Series], key_prefix:str="", auto_key_prefix="_", chunk_suffix:Union[bool,None]=None)->pd.Series:
+def extract_fasta_header_like(header:Union[str,list,pd.Series], key_prefix:str="", auto_key_prefix="_", chunk_suffix:Union[bool,None]=None)->pd.Series:
     """Function to extract information in a FASTA-like header as produced by the "extract_genes" functions. If a list-like of headers is given, it assumes the same structure for ever header.
 
     Args:
@@ -124,11 +56,34 @@ def extract_fasta_header_like_series(header:Union[str,list,pd.Series], key_prefi
         chunk_range = _header_split.loc[:,1].str.split("_").apply(pd.Series).iloc[:,1].str.split("-").apply(pd.Series)
         chunk_range.columns = [f"{key_prefix}{auto_key_prefix}chunk_start", f"{key_prefix}{auto_key_prefix}chunk_end"]
 
-    # Split the header by spaces
-    info = header.loc[:,"_hdr"].str.split(" ").apply(pd.Series)
+    # Split the header by certain spaces
+    # Replace first occurrence of space
+    sub_first = partial(re.sub, "^([^ ]+) ", "\\1___")
+    header.loc[:,"_hdr"] = header.loc[:,"_hdr"].apply(sub_first)
+
+    # Replace space before keys
+    sub_pairs = partial(re.sub, " ([^ =]+=)", "___\\1")
+    header.loc[:,"_hdr"] = header.loc[:,"_hdr"].apply(sub_pairs)
+
+    # Split by the introduced spacer to extract the information fields
+    info = header.loc[:,"_hdr"].str.split("___").apply(pd.Series)
+
+    # Create a container for the column names
+    col_names = np.full(info.shape[1], "", dtype="U64")
+
+    # Set the first column name as id
+    col_names[0] = f"{key_prefix}{auto_key_prefix}id"
 
     # Determine the column names form the first row
-    col_names = [f"{auto_key_prefix}id"] + info.iloc[0,1:].str.split("=").apply(pd.Series).iloc[:,0].to_list()
+    is_keypair = [False] + (info.iloc[0,1:].str.find("=") > 0).to_list()
+
+    # Extract the key names from the keypair columns
+    col_names[is_keypair] = info.iloc[0, is_keypair].str.split("=").apply(pd.Series).iloc[:,0].to_list()
+
+    # Set general names for the remaining columns
+    is_remaining = np.invert(is_keypair[1:])
+    if np.any(is_remaining):
+        col_names[1:][is_remaining] = [f"{key_prefix}{auto_key_prefix}descriptor{i if i>0 else ''}" for i in range(np.sum(is_remaining))]
     info.columns = [key_prefix + x for x in col_names]
 
     # Remove the prefixes
