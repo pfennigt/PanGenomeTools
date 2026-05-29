9e0afbde6f552425eb1b302eff6adfdd5b254f8c
Tobias Pfennig <pfennig@ipk-gatersleben.de>
2026-05-29 17:18:35 +0200

    updated test fasta sequence and gff

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,21 +20,26 @@ def setup_test_files(temp_test_dir):
     # Create GFF file
     gff_content = """##gff-version 3
 chr1	.	gene	100	500	.	+	.	ID=GENE001
-chr1	.	CDS	150	200	.	+	.	ID=CDS001;Parent=GENE001
-chr1	.	CDS	300	350	.	+	.	ID=CDS002;Parent=GENE001
-chr1	.	CDS	400	450	.	+	.	ID=CDS003;Parent=GENE001
+chr1	.	mRNA	100	500	.	+	.	ID=GENE001.1;Parent=GENE001
+chr1	.	CDS	150	200	.	+	.	ID=CDS001;Parent=GENE001.1
+chr1	.	CDS	300	350	.	+	.	ID=CDS002;Parent=GENE001.1
+chr1	.	CDS	400	450	.	+	.	ID=CDS003;Parent=GENE001.1
 chr1	.	gene	600	1000	.	-	.	ID=GENE002
-chr1	.	CDS	650	700	.	-	.	ID=CDS004;Parent=GENE002
-chr1	.	CDS	800	850	.	-	.	ID=CDS005;Parent=GENE002
+chr1	.	mRNA	600	1000	.	-	.	ID=GENE002.1;Parent=GENE002
+chr1	.	CDS	650	700	.	-	.	ID=CDS004;Parent=GENE002.1
+chr1	.	CDS	800	850	.	-	.	ID=CDS005;Parent=GENE002.1
 """
 
     # Create index file
     index_content = """genotype,annotation,assembly
-test1,test.gff,test.fa
+test1,test.gff3,test.fa
 """
 
-    # Create FASTA file
-    fasta_content = ">chr1\n" + "ATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCG\n" * 20
+    # Create a pseudo FASTA file 
+    # fasta_content = ">chr1\n" + "ATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCG\n" * 20
+    _fasta_seq = "".join([c*i for i in range(1,25) for c in "ACGT"])
+    fasta_content = ">chr1\n" + "\n".join([_fasta_seq[i*80:(i+1)*80] for i in range(int(len(_fasta_seq)/80))])
+
 
     # Create target genes file
     target_content = """gene_name,gene_ID_test1
@@ -42,7 +47,7 @@ GENE001,GENE001
 GENE002,GENE002"""
 
     # Write files
-    (temp_test_dir / "test.gff").write_text(gff_content)
+    (temp_test_dir / "test.gff3").write_text(gff_content)
     (temp_test_dir / "index.csv").write_text(index_content)
     (temp_test_dir / "test.fa").write_text(fasta_content)
     (temp_test_dir / "target_genes.csv").write_text(target_content)
